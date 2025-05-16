1adb4f8b69e2f330aa1765163a908bae6bed3dea
Murilo Rodrigues <murilooliveira10789@gmail.com>
2025-05-16 19:02:53 -0300

    Adiconando acesso com banco de dados

--- src/model/services/DepartamentService.java
+++ src/model/services/DepartamentService.java
@@ -1,19 +1,19 @@
 package model.services;
 
-import java.util.ArrayList;
+
 import java.util.List;
 
+import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
 import model.entities.Departament;
 
 public class DepartamentService {
 	
 	
+	private DepartmentDao dao = DaoFactory.createDepartmentDao();
+	
 	public List<Departament> findAll(){
-		List<Departament> list = new ArrayList<>();
-		list.add(new Departament(1,"Books"));
-		list.add(new Departament(2,"Computers"));
-		list.add(new Departament(3,"Eletronics"));
-		return list;
+		return dao.findAll();
 	}
 
 }
